8041438702a0f85c572d4ff51122cf7461103b46
Don Davis <dcaa2005@hotmail.com>
2018-09-11 14:41:01 -0600

    store udpated for user auth

--- client/src/store.js
+++ client/src/store.js
@@ -6,7 +6,7 @@ import router from './router'
 Vue.use(Vuex)
 
 let auth = Axios.create({
-  baseURL: 'localhost:3000/auth',
+  baseURL: 'localhost:3000/auth/',
   timeout: 3000,
   withCredentials: true
 })
@@ -23,17 +23,43 @@ export default new Vuex.Store({
   },
   mutations: {
 
+    setUser(state, user) {
+      state.user = user;
+    }
+
   },
   actions: {
 
     //USER AUTH METHODS
 
     loginUser({ commit, dispatch }, creds) {
+      auth.post('login', creds)
+        .then(res => {
+          console.log('login user return: ', res.data)
+          commit('setUser', res.data)
+          router.push({ name: 'home' })
+        })
+        .catch(err => console.error(err))
 
     },
 
     registerUser({ commit, dispatch }, newUser) {
+      auth.post('register', newUser)
+        .then(res => {
+          console.log("register user return: ", res.data)
+          commit('setUser', res.data)
+          router.push({ name: 'home' })
+        })
+        .catch(err => console.error(err))
+    },
 
+    authenticate({ commit, dispatch }) {
+      auth.get('authenticate')
+        .then(res => {
+          commit('setUser', res.data)
+          router.push({ name: 'home' })
+        })
+        .catch(err => console.error(err))
     }
 
 
