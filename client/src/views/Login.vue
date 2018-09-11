63c296ed31ff1386bf07d4f7aa55b45705a27b54
Porter Wilcox <wilcox.porter@gmail.com>
2018-09-11 14:41:58 -0600

    conditional sign in forms

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,22 +1,22 @@
 <template>
-  <div id="welcome-page">
-    <div id="login">
-      <form v-if="!newUser" @submit.prevent="loginUser" class="form-group">
+  <div class="login">
+    <div class="login-form" v-if="newUser">
+      <form @submit.prevent="loginUser" class="form-group">
         <input class="form-control" type="text" name="username" placeholder="User Name" required v-model="creds.username">
         <input class="form-control" type="password" name="password" placeholder="Password" required v-model="creds.password">
         <button type="submit" class="btn btn-primary">Login</button>
       </form>
-      <h5><span @click="newUser = true" class="clickable">Register for an account</span></h5>
+      <h5><span @click="newUser = !newUser" class="clickable">Register for an account</span></h5>
     </div>
-    <div id="register">
-      <form v-if="newUser" @submit.prevent="registerUser" class="form-group">
+    <div class="register-form" v-else>
+      <form @submit.prevent="registerUser" class="form-group">
         <input class="form-control" type="email" name="email" placeholder="email address" required v-model="newUser.email">
         <input class="form-control" type="text" name="username" placeholder="User Name" required v-model="newUser.username">
         <input class="form-control" type="password" name="password" placeholder="Password" required v-model="newUser.password">
         <input class="form-control" type="password" name="password2" placeholder="Re-enter Password" required v-model="newUser.password2">
         <button type="submit" class="btn btn-primary">Register</button>
       </form>
-      <h5><span @click="newUser = false" class="clickable">Login to existing account</span></h5>
+      <h5><span @click="newUser = !newUser" class="clickable">Login to existing account</span></h5>
     </div>
   </div>
 </template>
@@ -27,7 +27,7 @@
     name: 'login',
     data() {
       return {
-        newUser: true,
+        newUser: false,
         newUser: {
           username: '',
           password: '',
