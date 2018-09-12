0c1019095c298de0267410dc541e0b4a1e253056
michael <michael.green94@hotmail.com>
2018-09-12 07:24:32 -0600

    worked on routes

--- server/server-assets/routes/posts.js
+++ server/server-assets/routes/posts.js
@@ -1,7 +1,7 @@
 let router = require('express').Router()
 let Post = require('../models/post')
 
-//get all posts
+//get all posts (possibly change)
 router.get('/', (req, res, next) => {
   Post.find({})
     .then(posts => {
@@ -10,7 +10,7 @@ router.get('/', (req, res, next) => {
     .catch(next)
 })
 
-//get specific posts
+//get specific posts(users posts?)
 router.get('/:id', (req, res, next) => {
   Post.findById(req.params.id)
     .then(post => {
@@ -19,10 +19,25 @@ router.get('/:id', (req, res, next) => {
     .catch(next)
 })
 
-//put
-//post
+//edit a post
+router.put('/:id', (req, res, next) => {
+  Post.findByIdAndUpdate(req.params.id, req.body)
+    .then(() => res.send({
+      message: 'Edited'
+    }))
+})
+
+//create a post
+router.post('/', (req, res, next) => {
+  Post.create(req.body)
+    .then(post => {
+      res.send(post)
+    })
+    .catch(next)
+})
+
 //delete
-router.delete('/:id', (req, res, send) => {
+router.delete('/:id', (req, res, next) => {
   Post.findByIdAndRemove(req.params.id)
     .then(() => res.send({
       message: 'Deleted'
