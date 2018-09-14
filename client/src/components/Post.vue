a396966f9f3776167580075aba34e51961f6c4d6
michael <michael.green94@hotmail.com>
2018-09-13 21:49:22 -0600

    styling posts

--- client/src/components/Post.vue
+++ client/src/components/Post.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="posts">
     <div class="post" v-for="post in posts" :key="post._id">
-      <h1>{{post.category}}</h1>
-      <p @click="showUser(post.userId)">{{post.userName}}</p>
-      <p>{{post.content}}</p>
-      <p>Within {{post.distance}} miles</p>
+      <div class="category">{{post.category}}</div>
+      <div class="userName" @click="showUser(post.userId)">{{post.userName}}</div>
+      <div class="distance">{{Math.round(post.distance)}} miles away</div>
+      <div class="content">{{post.content}}</div>
       <div class="votes">
-      <i @click="upvoted = !upvoted" v-if="upvoted" class="fas fa-arrow-alt-circle-up"></i>
-      <i @click="upvoted = !upvoted" v-else class="far fa-arrow-alt-circle-up"></i>
-      <i @click="downvoted = !downvoted" v-if="downvoted" class="fas fa-arrow-alt-circle-down"></i>
-      <i @click="downvoted = !downvoted" v-else class="far fa-arrow-alt-circle-down"></i>
+        <i @click="upvoted = !upvoted" v-if="upvoted" class="fas fa-arrow-alt-circle-up"></i>
+        <i @click="upvoted = !upvoted" v-else class="far fa-arrow-alt-circle-up"></i>
+        <i @click="downvoted = !downvoted" v-if="downvoted" class="fas fa-arrow-alt-circle-down"></i>
+        <i @click="downvoted = !downvoted" v-else class="far fa-arrow-alt-circle-down"></i>
       </div>
     </div>
   </div>
@@ -40,7 +40,43 @@ export default {
 </script>
 
 <style scoped>
-/* * {
+* {
   outline: 1px solid red;
-} */
+}
+
+.posts {
+  /* width: 50%; */
+  height: auto;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.post {
+  width: 100%;
+  display: flex;
+  height: auto;
+  border: 1px solid black;
+  flex-wrap: wrap;
+  flex-direction: row;
+}
+
+.category {
+  width: 50%;
+}
+
+.username {
+  width: 25%;
+}
+
+.content {
+  width: 100%;
+}
+
+.distance {
+  width: 25%;
+}
+
+.votes {
+  width: 100%;
+}
 </style>
