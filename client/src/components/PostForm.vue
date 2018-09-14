4c4d8134ea718e2afc09519bcd90cf88ea87e444
Porter Wilcox <wilcox.porter@gmail.com>
2018-09-14 14:28:45 -0600

    v-validate radio group

--- client/src/components/PostForm.vue
+++ client/src/components/PostForm.vue
@@ -5,12 +5,14 @@
         <i v-else slot="activator" @click="addPost = !addPost" id="minus-form" class="btn btn-danger fas fa-plus add-post-btn"></i>
         <v-card>
             <v-card-title id="form-title">create a post</v-card-title>
-            <form @submit.prevent="verifyPost" id="form-body">
-                <v-text-field v-model="post.title" maxlength="30" placeholder="title" type="string" required 
-                v-validate="'required|max:30'"  data-vv-name="val.title" :error-messages="errors.collect('val.title')"></v-text-field> 
-                <v-textarea v-model="post.content" outline maxlength="140" counter placeholder="post content" clearable clear-icon="X" type="string" required></v-textarea> 
-                <v-radio-group v-model="post.category" label="#category">
-                    <v-radio append-icon="this" v-for="c in catOptions" :key="c" :label="c" :value="c" required></v-radio>
+            <form id="form-body">
+                <v-text-field v-model="post.title" :counter="30" placeholder="title" type="string" required 
+                v-validate="'required|max:30'"  data-vv-name="title" :error-messages="errors.collect('title')"></v-text-field> 
+                <v-textarea v-model="post.content" outline :counter="140" placeholder="post content" clearable clear-icon="X" type="string" required
+                v-validate="'required|max:140'" data-vv-name="content" :error-messages="errors.collect('content')"></v-textarea> 
+                <v-radio-group v-model="post.category" label="#category"
+                v-validate="'required|included:0,1,2,3'" data-vv-name="category" :error-messages="errors.collect('category')">
+                    <v-radio append-icon="this" v-for="(c, i) in catOptions" :key="c" :label="c" :value="i" required></v-radio>
                 </v-radio-group>
                 <v-flex justify-content-center>
                     <v-btn @click="submit" round color="#18bc9c">submit</v-btn>
@@ -46,6 +48,13 @@ export default {
           title: {
             required: () => "Every post needs a title",
             max: "Title cannot be longer than 30 characters"
+          },
+          content: {
+            required: () => "Every post needs a content body",
+            max: "Content cannot be longer than 140 characters"
+          },
+          category: {
+            required: () => "Every post needs a category",
           }
         }
       }
@@ -57,10 +66,13 @@ export default {
 
   methods: {
     submit() {
-      this.$validator.validateAll();
-    },
-    verifyPost(){
-        console.log(this.post.content)
+      this.$validator.validateAll().then(res => {
+        if(!res){
+          return 
+        }
+        this.post.category = this.catOptions[this.post.category]
+        this.createPost()
+      })
     }
   }
 };
