1ce222531bc6f2b8cd3b4d86777385473e09dbcd
Don Davis <dcaa2005@hotmail.com>
2018-09-17 13:49:22 -0600

    minor updates to post format

--- client/src/components/Post.vue
+++ client/src/components/Post.vue
@@ -26,183 +26,183 @@
   </div>
 </template>
 <script>
-let moment = require("moment");
+  let moment = require("moment");
 
-export default {
-  name: "post",
-  data() {
-    return {
-      datavote: {
-        upvoted: false,
-        downvoted: false
+  export default {
+    name: "post",
+    data() {
+      return {
+        datavote: {
+          upvoted: false,
+          downvoted: false
+        },
+        voted: {
+          vote: 1
+        },
+        dVoted: {
+          vote: -1
+        }
+      };
+    },
+    methods: {
+      showUser() {
+        //some stuff
       },
-      voted: {
-        vote: 1
+      upVote(id) {
+        // debugger;
+        this.$store.dispatch("vote", { postId: id, vote: this.voted });
+        // debugger;
       },
-      dVoted: {
-        vote: -1
+      downVote(id) {
+        this.$store.dispatch("vote", { postId: id, vote: this.dVoted });
+      },
+      calculateVotes(obj) {
+        if (!obj) {
+          return 0;
+        }
+        let out = "";
+        let postVotes = Object.values(obj);
+        const getSum = (sum, value) => sum + value;
+        let totalVotes = postVotes.reduce(getSum);
+        if (totalVotes < 0) {
+          return (out = "SUSPECT");
+        } else if (totalVotes >= 2) {
+          return (out = "VERIFIED");
+        } else return (out = "UNVERIFIED");
       }
-    };
-  },
-  methods: {
-    showUser() {
-      //some stuff
     },
-    upVote(id) {
-      // debugger;
-      this.$store.dispatch("vote", { postId: id, vote: this.voted });
-      // debugger;
-    },
-    downVote(id) {
-      this.$store.dispatch("vote", { postId: id, vote: this.dVoted });
-    },
-    calculateVotes(obj) {
-      if (!obj) {
-        return 0;
-      }
-      let out = "";
-      let postVotes = Object.values(obj);
-      const getSum = (sum, value) => sum + value;
-      let totalVotes = postVotes.reduce(getSum);
-      if (totalVotes < 0) {
-        return (out = "SUSPECT");
-      } else if (totalVotes >= 2) {
-        return (out = "VERIFIED");
-      } else return (out = "UNVERIFIED");
-    }
-  },
 
-  computed: {
-    posts() {
-      return this.$store.state.activePosts;
+    computed: {
+      posts() {
+        return this.$store.state.activePosts;
+      }
     }
-  }
-};
+  };
 </script>
 
 <style scoped>
-/* * {
+  /* * {
   outline: 1px solid red;
 } */
 
-.spacer {
-  height: 25vh;
-  color: transparent;
-  pointer-events: none;
-}
+  .spacer {
+    height: 25vh;
+    color: transparent;
+    pointer-events: none;
+  }
 
-.posts {
-  width: 70%;
-  height: 100%;
-  display: flex;
-  flex-wrap: wrap;
-}
+  .posts {
+    width: 70%;
+    height: 100%;
+    display: flex;
+    flex-wrap: wrap;
+  }
 
-p {
-  margin-bottom: 0;
-}
+  p {
+    margin-bottom: 0;
+  }
 
-.p-title {
-  text-transform: uppercase;
-  font-size: 2rem;
-}
+  .p-title {
+    text-transform: uppercase;
+    font-size: 2rem;
+  }
 
-.post {
-  width: 100%;
-  display: flex;
-  height: auto;
-  border-radius: 1rem;
-  flex-wrap: wrap;
-  flex-direction: row;
-  margin: 0.5rem;
-  /* background-color: #18bc9c; */
-  transition: 0.2s;
-}
+  .post {
+    width: 100%;
+    display: flex;
+    height: auto;
+    border-radius: 1rem;
+    flex-wrap: wrap;
+    flex-direction: row;
+    margin: 0.5rem;
+    /* background-color: #18bc9c; */
+    transition: 0.2s;
+  }
 
-.post:hover {
-  box-shadow: -4px 5px 24px 1px white;
-}
+  .post:hover {
+    box-shadow: -4px 5px 24px 1px white;
+  }
 
-.category {
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  /* padding: 1rem; */
-  background-color: #76828e;
-  /* margin-bottom: 1rem; */
-  border-top-left-radius: 1rem;
-  border-top-right-radius: 1rem;
-  /* border-bottom: 1px solid black; */
-}
+  .category {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    /* padding: 1rem; */
+    background-color: #76828e;
+    /* margin-bottom: 1rem; */
+    border-top-left-radius: 1rem;
+    border-top-right-radius: 1rem;
+    /* border-bottom: 1px solid black; */
+  }
 
-.distance {
-  padding-left: 1rem;
-}
+  .distance {
+    padding-left: 1rem;
+  }
 
-.userName {
-  width: 50%;
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  padding: 1rem;
-  font-size: 1rem;
-  color: white;
-}
+  .userName {
+    width: 50%;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    padding: 1rem;
+    font-size: 1rem;
+    color: #2C3E50;
+  }
 
-.u-nme:hover {
-  text-decoration: underline;
-  cursor: pointer;
-}
+  .u-nme:hover {
+    text-decoration: underline;
+    cursor: pointer;
+  }
 
-.userName p {
-  padding: 0 0.5rem 0 0.5rem;
-}
+  .userName p {
+    padding: 0 0.5rem 0 0.5rem;
+  }
 
-.content-holder {
-  width: 100%;
-  height: auto;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border: 1px solid #18bc9c;
-  padding-top: 1rem;
-  flex-wrap: wrap;
-  border-bottom-left-radius: 1rem;
-  border-bottom-right-radius: 1rem;
-  border-top: none;
-}
+  .content-holder {
+    width: 100%;
+    height: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid #18bc9c;
+    padding-top: 1rem;
+    flex-wrap: wrap;
+    border-bottom-left-radius: 1rem;
+    border-bottom-right-radius: 1rem;
+    border-top: none;
+  }
 
-.content {
-  width: 95%;
-  height: auto;
-  text-align: left;
-  padding: 0.5rem;
-  border: 1px solid grey;
-  /* border-radius: 0.5rem; */
-  background-color: #ecf0f1;
-}
+  .content {
+    width: 95%;
+    height: auto;
+    text-align: left;
+    padding: 0.5rem;
+    border: 1px solid grey;
+    /* border-radius: 0.5rem; */
+    background-color: #ecf0f1;
+  }
 
-/* .distance {
+  /* .distance {
   width: 25%;
 } */
 
-.votes {
-  width: 50%;
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  padding: 1rem;
-  font-size: 1rem;
-  color: white;
-}
+  .votes {
+    width: 50%;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    padding: 1rem;
+    font-size: 1rem;
+    color: #2C3E50;
+  }
 
-.votes i {
-  padding: 0 0.5rem 0 0;
-  cursor: pointer;
-}
+  .votes i {
+    padding: 0 0.5rem 0 0;
+    cursor: pointer;
+  }
 
-/* .time {
+  /* .time {
   width: 50%;
 } */
 </style>
