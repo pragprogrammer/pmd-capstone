015714b561e418e80a41f71acc34c4546fcee2af
Porter Wilcox <wilcox.porter@gmail.com>
2018-09-18 16:06:25 -0600

    merge conflict

--- client/src/components/UserProfile.vue
+++ client/src/components/UserProfile.vue
@@ -1,85 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <v-layout class="user-profile">
-    <v-dialog v-model="showUserProfile" fullscreen transition="scale-transition">
-      <p @click="getTargetUser" class="u-nme" slot="activator"><strong>{{post.userName}}</strong></p>
-      <v-card>
-        <v-toolbar>
-          <v-btn @click.native="showUserProfile = false">
-            <v-icon>close</v-icon>
-          </v-btn>
-          <v-toolbar-title>{{post.userName}}</v-toolbar-title>
-        </v-toolbar>
-        <div v-if="user.username" class="profile-content">
-          <h2 class="mt-1">AGE</h2>
-          <span class="days-old">
-            <v-icon>fa-birthday-cake</v-icon>
-            <p>{{user.created | daysOld}}</p>
-          </span>
-          <p class="created">Member since: {{user.created | moment("MMMM Do, YYYY")}}</p>
-          <hr>
-          <h2>RELIABILITY</h2>
-          <div class="progresses">
-            <v-progress-circular v-if="user.reliability <= 10" color="#ff5252" size="150" :width="20" :value="user.reliability"
-              :rotate="90">{{user.reliability}}</v-progress-circular>
-            <v-progress-circular v-if="user.reliability <= 25 && user.reliability > 10" color="#ffc107" size="150"
-              :width="20" :value="user.reliability" :rotate="90">{{user.reliability}}</v-progress-circular>
-            <v-progress-circular v-if="user.reliability <= 60 && user.reliability > 25" color="#2196f3" size="150"
-              :width="20" :value="user.reliability" :rotate="90">{{user.reliability}}</v-progress-circular>
-            <v-progress-circular v-if="user.reliability <= 100 && user.reliability > 60" color="#4caf50" size="150"
-              :width="20" :value="user.reliability" :rotate="90">{{user.reliability}}</v-progress-circular>
-          </div>
-          <hr>
-          <div class="user-action">
-            <h2>DIRECT MESSAGE</h2>
-            <v-icon class="clickable">fa-envelope</v-icon>
-          </div>
-          <hr>
-          <div class="user-action">
-            <h2>BLOCK USER</h2>
-            <v-icon @click="blockUser(user.userId)" class="clickable">fa-ban</v-icon>
-          </div>
-        </div>
-      </v-card>
-    </v-dialog>
-  </v-layout>
-</template>
-
-<script>
-  let moment = require("moment");
-
-  export default {
-    name: "userProfile",
-    props: ["post"],
-    data() {
-      return {
-        showUserProfile: false
-      };
-    },
-    computed: {
-      user() {
-        return this.$store.state.targetUser;
-      }
-    },
-    methods: {
-      getTargetUser() {
-        this.$store.dispatch("getTargetUser", this.post.userId);
-      },
-
-      blockUser(userId) {
-        console.log("target user id ", userId)
-        this.$store.dispatch('blockUser', userId)
-      }
-    },
-    filters: {
-      daysOld(date) {
-        return moment(date).fromNow(true);
-      }
-    }
-
-
-  };
-=======
     <v-layout class="user-profile">
         <v-dialog v-model="showUserProfile" fullscreen transition="scale-transition">
             <p @click="getTargetUser" class="u-nme" slot="activator"><strong>{{post.userName}}</strong></p>
@@ -149,7 +68,6 @@ export default {
     ContactUser
   }
 };
->>>>>>> 5555bb7f357e9d8a8cf5985540ec3ac0f053377d
 </script>
 
 <style scoped>
